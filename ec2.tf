345328ada5173b12003d7b4e0b8430838f8ee057
Rameshwar Korde <101036700+Ram061996@users.noreply.github.com>
2022-04-14 15:58:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2_demo" {
   ami                    = "ami-04893cdb768d0f9ee"
   instance_type          = "t2.micro"
-  key_name               = "demo"
+  key_name               = "demo_pur"
   subnet_id              = "subnet-0e900c1f8a8c9bd5e"
   vpc_security_group_ids = [aws_security_group.ec2_demo_sg.id]
 
